e743c4001500bd34e961eee323fe2d00bacc2d12
Priya Agarwal <p.agarwal@globallogic.com>
2013-07-26 10:01:08 +0530

    init

--- server.js
+++ server.js
@@ -6,27 +6,27 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
-// var port = process.env.PORT || 5000;
-// app.listen(port, function() {
-  // console.log("Listening on " + port);
-// });
-
-var express = require('express');
-var app = express();
-
-app.set("view options", {layout: false});
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(req, res) {
-    res.render('index.html');
-});
-
-// spin up server
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
+ var port = process.env.PORT || 5000;
+ app.listen(port, function() {
    console.log("Listening on " + port);
  });
-app.use(express.static(__dirname + '/public'));
+
+// var express = require('express');
+// var app = express();
+// 
+// app.set("view options", {layout: false});
+// app.use(express.static(__dirname + '/public'));
+// 
+// app.get('/', function(req, res) {
+    // res.render('index.html');
+// });
+
+// spin up server
+// var port = process.env.PORT || 5000;
+// app.listen(port, function() {
+   // console.log("Listening on " + port);
+ // });
+// app.use(express.static(__dirname + '/public'));
 //var index = fs.readFileSync('./views/index.html');
 
 // http.createServer(function (req, res) {
